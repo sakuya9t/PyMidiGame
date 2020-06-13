033bf393157e6ffd51677227b43b6611a781e854
sakuya9t <sakuya9t@hotmail.com>
2020-06-13 15:15:40 -0400

    mp3 player module

--- midi/MusicPlayer.py
+++ midi/MusicPlayer.py
@@ -1,10 +1,15 @@
+import midi.mp3play as mp3play
+
+clip = mp3play.load(r'C:\Users\sakuya\Music\AGA - Wonderful U.mp3')
+
+clip.play()
+
+# Let it play for up to 30 seconds, then stop it.
 import time
-
-import pygame
-
-pygame.mixer.pre_init(44100)  # setup mixer to avoid sound lag
-pygame.init()
-pygame.mixer.init()
-pygame.mixer.music.load('../resources/chords.mid')
-pygame.mixer.music.play()
-time.sleep(60)
+time.sleep(min(5, clip.seconds()))
+clip.pause()
+time.sleep(5)
+clip.unpause()
+time.sleep(5)
+clip.stop()
+print('a')
---
+++ midi/mp3play/readme.txt
@@ -0,0 +1,2 @@
+Mp3play module:
+folked from https://github.com/michaelgundlach/mp3play
